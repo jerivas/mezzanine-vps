e09fac6fa98a6ef9fce74bdd767ee1ed38d1f2e3
Eduardo Rivas <e@jerivas.com>
2014-09-03 21:18:42 -0600

    Make the user created by vps_primer.sh a variable argument. Also, add many introductory comments.

--- vps_primer.sh
+++ vps_primer.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Run this script first thing when you get your VPS. It assumes you're logged
+# in as root and is necessary for the rest of the scripts to run.
+
+# This should be the only task you execute as root, when the script finishes,
+# you should logout and log back in with the user created by the script.
+
+# USAGE
+# 1. Make the script executable:
+#     $ chmod +x vps_primer.sh
+# 2. Run the script. The only argument is the user you want to create.
+#     $ ./vps_primer.sh webapps
+#     (runs the script and creates a user "webapps")
+
 # Simple function to colorize output
 # http://stackoverflow.com/a/23006365/1330003
 function echolor() {
@@ -23,18 +36,21 @@ function echolor() {
     tput sgr0;
 }
 
+# THE SCRIPT
+# Not commenting much as all this stuff is self-explanatory
 echolor "Setting up locales." blue
-locale-gen en_US.UTF-8 es_ES.UTF-8 es_US.UTF-8 es_SV.UTF-8
+# Non-english speaking users can add additional languages here
+locale-gen en_US.UTF-8
 update-locale en_US.UTF-8
 echolor "Updating system packages. This may take a while." blue
 apt-get update -q > /dev/null
 apt-get upgrade -y -q > /dev/null
 echolor "Installing 'nano' text editor." blue
-apt-get install -q nano > /dev/null
-echolor "Creating user jerivas with sudo privileges." blue
-adduser jerivas
-usermod -G sudo jerivas
-echolor "Disabling Root login." blue
+apt-get install -y -q nano > /dev/null
+echolor "Creating user $1 with sudo privileges." blue
+adduser $1
+usermod -G sudo $1
+echolor "Disabling Root login via SSH." blue
 sed -i "s:RootLogin yes:RootLogin no:" /etc/ssh/sshd_config
 service ssh restart
-echolor "Done. Log out and log in as jerivas from now on." blue
+echolor "Done. Log out and log in as $1 from now on." green
